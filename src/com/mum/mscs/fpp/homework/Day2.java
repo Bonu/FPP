fe8b7374c0338d954ea88e5babab619b3679f60c
Janardhan Bonu <janardhanbonu@gmaill.com>
2015-03-17 14:46:29 -0500

    update

---
+++ src/com/mum/mscs/fpp/homework/Day2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.mum.mscs.fpp.homework;
+
+/**
+ * @author 984417
+ *
+ */
+public class Day2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Day 1 - Homework");
+	}
+
+}
