c89fbfe65043f1e6b3ca6be4fe7344544720ad96
Janardhan Bonu <janardhanbonu@gmaill.com>
2015-03-19 18:02:24 -0500

    -

--- src/com/mum/mscs/fpp/homework/practise/Rhombus.java
+++ src/com/mum/mscs/fpp/homework/practise/Rhombus.java
@@ -36,26 +36,49 @@ public class Rhombus {
 		int num = 5;
 		int refVal = (num/2)+1;
 		int refVal1 = refVal;
-		int refVal2 = refVal;
-		
+		int refVal2 = 1;
+		int val1 = 1;
 		System.out.println(num);
 		System.out.println(refVal);
-		for(int a=1;a<=num;a++) {
-			for(int i=1;i<= refVal1;i++){
-				System.out.printf("-");
-				refVal1 = refVal1 - 1;
+		
+		for(int a=1;a<=refVal;a++) {
+			for(int i=1; i<= refVal1; i++){
+				System.out.printf(" ");
 			}
+			refVal1--;
 			
-			for(int b=refVal2;b >= 0 ;) {
+			for(int j=1;j<=val1;j++){
 				System.out.printf("*");
-				refVal2 = refVal2 + 1;
-				b = b  - 2;
 			}
-			num = num -2;
-			System.out.println("");
+			val1 = val1 + 2;
+			System.out.println(" ");
 		}
+		
+		num = num - 2;
+		refVal = (num/2)+1;
+		refVal1 = refVal;
+		refVal2 = 1;
+		val1 = 1;
+		System.out.println(num);
+		System.out.println(refVal);
+		
+		for(int a=1;a<=refVal;a++) {
+			for(int i=1; i<= refVal1; i++){
+				System.out.printf(" ");
+			}
+			refVal1--;
+			for(int j=1;j<=val1;j++){
+				System.out.printf("*");
+			}
+			val1 = val1 + 2;
+			System.out.println(" ");
+		}
+		
+
 		
 		
 	}
+	
+	
 
 }
