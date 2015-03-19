585a5a168385dfe5521486cd843f18d08940fead
janardhanbonu <janardhanbonu@gmail.com>
2015-03-18 23:16:20 -0500

    updated

---
+++ src/com/mum/mscs/fpp/homework/w1d3/AgeCalculator.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.mum.mscs.fpp.homework.w1d3;
+
+/**
+ * @author janardhanbonu
+ *
+ */
+public class AgeCalculator {
+
+	/**
+	 * 
+	 */
+	public AgeCalculator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
