5283a7f2602dc56d968d68de1a27f5c1e2a870c5
Janardhan Bonu <janardhanbonu@gmaill.com>
2015-03-19 11:47:42 -0500

    updated changes

--- src/com/mum/mscs/fpp/homework/w1d3/Account.java
+++ src/com/mum/mscs/fpp/homework/w1d3/Account.java
@@ -8,12 +8,12 @@ package com.mum.mscs.fpp.homework.w1d3;
 		Account
 		+balance : double
 		+double : getBalance()
-		+int credit()
-		+int debit() 
+		+int credit(amount)
+		+int debit(amount) 
 		
 		In the above class diagram the methods credit() and debit() return -1 
 		for the negative or zero input and also display the information as 
-		“Invalid credit/debit amount” else return 1.
+		Invalid credit/debit amount
 		
 		output:
 		Invalid Debit amount
@@ -31,19 +31,22 @@ package com.mum.mscs.fpp.homework.w1d3;
 	public double balance;
 	
 	public double getBalance(){
-		System.out.println("Balance Amount:"+balance);
+		System.out.printf("Balance Amount: %.2f\n",balance);
 		return balance;
 	}
 	
-	public short credit(){
+	public short credit(double amount){
+		this.balance = this.balance - amount;
 		return checkBalance("Credit");
 	}
 	
-	public short debit() {
+	public short debit(double amount) {
+		this.balance = this.balance + amount;
 		return checkBalance("Debit");
 	}
 	
 	public short checkBalance(String transactionType){
+		
 		if(balance < 0){
 			System.out.printf("Invalid %s amount\n", transactionType);
 			return -1;
@@ -51,6 +54,7 @@ package com.mum.mscs.fpp.homework.w1d3;
 			System.out.printf("Invalid %s amount\n", transactionType);
 			return 0;
 		} else {
+			System.out.printf("Amount %sed\n", transactionType);
 			return 1;
 		}
 	}
@@ -61,13 +65,12 @@ package com.mum.mscs.fpp.homework.w1d3;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Account acnt1 = new Account(0l);
-		acnt1.debit();
+		Account acnt1 = new Account(20000);
+		acnt1.debit(1000);
 		acnt1.getBalance();
 		
-		
-		Account acnt2 = new Account(100l);
-		acnt2.credit();
+		Account acnt2 = new Account(1000);
+		acnt2.credit(1000);
 		acnt2.getBalance();
 		
 	}
