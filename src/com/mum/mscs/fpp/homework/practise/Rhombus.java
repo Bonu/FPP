41d0ec3fb8fdf7f5f1a8810d27770cb923dda485
janardhanbonu <janardhanbonu@gmail.com>
2015-03-19 20:21:28 -0500

    Finish

--- src/com/mum/mscs/fpp/homework/practise/Rhombus.java
+++ src/com/mum/mscs/fpp/homework/practise/Rhombus.java
@@ -31,50 +31,75 @@ public class Rhombus {
 		---*         ---        *
 		
 	 * 
+	 * Output: 
+	 *  1
+		-* 
+		3
+		--* 
+		-*** 
+		--* 
+		5
+		---* 
+		--*** 
+		-***** 
+		--*** 
+		---* 
+		7
+		----* 
+		---*** 
+		--***** 
+		-******* 
+		--***** 
+		---*** 
+		----* 
+		9
+		-----* 
+		----*** 
+		---***** 
+		--******* 
+		-********* 
+		--******* 
+		---***** 
+		----*** 
+		-----* 
 	 * The Rhombus 
 	 * @param args
 	 */
 	
 	public void displayRhombus(int num){
-		// TODO Auto-generated method stub
-//				int refVal = (num/2)+1;
-//				int refVal1 = refVal;
-//				int refVal2 = 1;
-//				int val1 = 1;
-//				System.out.println(num);
-////				System.out.println(refVal);
-//				
-//				for(int a=1;a<=refVal;a++) {
-//					for(int i=1; i<= refVal1; i++){
-//						System.out.printf(" ");
-//					}
-//					refVal1--;
-//					
-//					for(int j=1;j<=val1;j++){
-//						System.out.printf("*");
-//					}
-//					val1 = val1 + 2;
-//					System.out.println(" ");
-//				}
-				
-				num = num - 2;
-				int refVal = (num/2)+1;
-				int refVal1 = refVal;
-				int refVal2 = 1;
-				int val1 = num;
-				
-				for(int a=1;a<=refVal;a++) {
-//					System.out.printf("Loop %d\n",a);
-					for(int i=1; i<= refVal1; i++){
-						System.out.printf("%d",a);
-					}
-					refVal1++;
-					for(int j=1;j<=val1;j++){
-						System.out.printf("*");
-					}
-					val1 = val1  - 2;
-					System.out.println(" ");
-				}
+		int refVal = (num/2)+1;
+		int refVal1 = refVal;
+		int val1 = 1;
+		System.out.println(num);
+		
+		for(int a=1;a<=refVal;a++) {
+			for(int i=1; i<= refVal1; i++){
+				System.out.printf("-");
+			}
+			refVal1--;
+			
+			for(int j=1;j<=val1;j++){
+				System.out.printf("*");
+			}
+			val1 = val1 + 2;
+			System.out.println(" ");
+		}
+		
+		num = num - 2;
+		refVal1 = 1;
+		val1 = num;
+		
+		for(int a=1;a<=refVal-1;a++) {
+			for(int i=1; i<=refVal1+1; i++){
+				System.out.printf("-");
+			}
+			refVal1++;
+			for(int j=1;j<=val1;j++){
+				System.out.printf("*");
+			}
+			val1 = val1  - 2;
+			System.out.println(" ");
+		}
 				
 	}
 	public static void main(String[] args) {
