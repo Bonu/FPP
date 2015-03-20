0193d14b9d2cae4b6df0a128e6afa325740e3d4b
janardhanbonu <janardhanbonu@gmail.com>
2015-03-19 20:39:17 -0500

    -

--- src/com/mum/mscs/fpp/homework/practise/Rhombus.java
+++ src/com/mum/mscs/fpp/homework/practise/Rhombus.java
@@ -32,36 +32,37 @@ public class Rhombus {
 		
 	 * 
 	 * Output: 
-	 *  1
-		-* 
+		1
+		 * 
 		3
-		--* 
-		-*** 
-		--* 
+		  * 
+		 *** 
+		  * 
 		5
-		---* 
-		--*** 
-		-***** 
-		--*** 
-		---* 
+		   * 
+		  *** 
+		 ***** 
+		  *** 
+		   * 
 		7
-		----* 
-		---*** 
-		--***** 
-		-******* 
-		--***** 
-		---*** 
-		----* 
+		    * 
+		   *** 
+		  ***** 
+		 ******* 
+		  ***** 
+		   *** 
+		    * 
 		9
-		-----* 
-		----*** 
-		---***** 
-		--******* 
-		-********* 
-		--******* 
-		---***** 
-		----*** 
-		-----* 
+		     * 
+		    *** 
+		   ***** 
+		  ******* 
+		 ********* 
+		  ******* 
+		   ***** 
+		    *** 
+		     * 
+
 	 * The Rhombus 
 	 * @param args
 	 */
@@ -72,9 +73,21 @@ public class Rhombus {
 		int val1 = 1;
 		System.out.println(num);
 		
+		
+		/*  The Rhombus diagram has 2 basic triangles and each has 2 triangles again.
+		 *  The upper triangle is made up of spaces and '*' symbols in a defined order
+		 *  of odd number. A traingle with empty space is created first by taking the 
+		 *  middle line of the triangle as reference point and decrease the number of 
+		 *  spaces one by one for the next lines.
+		 *  In the same iteration create a loop to print * and add more * for next line 
+		 *  in odd number sequence.
+		 *  
+		 *  the lower triangle is made of space and '*' symbols in a decreasing order 
+		 *  compare to the upper triangle.
+		 */
 		for(int a=1;a<=refVal;a++) {
 			for(int i=1; i<= refVal1; i++){
-				System.out.printf("-");
+				System.out.printf(" ");
 			}
 			refVal1--;
 			
@@ -91,7 +104,7 @@ public class Rhombus {
 		
 		for(int a=1;a<=refVal-1;a++) {
 			for(int i=1; i<=refVal1+1; i++){
-				System.out.printf("-");
+				System.out.printf(" ");
 			}
 			refVal1++;
 			for(int j=1;j<=val1;j++){
