2d65e1e7a03288078d44f007860feca90f19eb4d
janardhanbonu <janardhanbonu@gmail.com>
2015-03-20 17:59:32 -0500

    updated sort

--- src/com/mum/mscs/fpp/homework/w1d3/AgeCalculator.java
+++ src/com/mum/mscs/fpp/homework/w1d3/AgeCalculator.java
@@ -3,6 +3,7 @@
  */
 package com.mum.mscs.fpp.homework.w1d3;
 
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -13,16 +14,31 @@ import java.util.GregorianCalendar;
 		b. Sort the birth dates. 
 		
   output:
-  	Date :Sat Apr 12 23:58:49 CST 1980	 Age :35
-	Date :Thu May 11 23:58:49 CDT 1950	 Age :65
-	Date :Sat Jun 10 23:58:49 CDT 1967	 Age :48
-	Date :Tue Jul 11 23:58:49 CDT 1978	 Age :37
-	Date :Thu Aug 17 23:58:49 CDT 1989	 Age :26
-	Date :Sun Sep 16 23:58:49 CDT 1990	 Age :25
-	Date :Sun Oct 25 23:58:49 CST 1998	 Age :17
-	Date :Sat Nov 21 23:58:49 CST 1987	 Age :28
-	Date :Wed Dec 22 23:58:49 CST 1976	 Age :39
-	Date :Sun Jan 23 23:58:49 CST 1966	 Age :49
+  
+  	Date :Sat Apr 12 17:53:40 CST 1980	 Age :35
+	Date :Thu May 11 17:53:40 CDT 1950	 Age :65
+	Date :Sat Jun 10 17:53:40 CDT 1967	 Age :48
+	Date :Tue Jul 11 17:53:40 CDT 1978	 Age :37
+	Date :Thu Aug 17 17:53:40 CDT 1989	 Age :26
+	Date :Sun Sep 16 17:53:40 CDT 1990	 Age :25
+	Date :Sun Oct 25 17:53:40 CST 1998	 Age :17
+	Date :Sat Nov 21 17:53:40 CST 1987	 Age :28
+	Date :Wed Dec 22 17:53:40 CST 1976	 Age :39
+	Date :Sun Jan 23 17:53:40 CST 1966	 Age :49
+	
+	---- dates are sorted-----
+	
+	Date :Thu May 11 17:53:40 CDT 1950
+	Date :Sun Jan 23 17:53:40 CST 1966
+	Date :Sat Jun 10 17:53:40 CDT 1967
+	Date :Wed Dec 22 17:53:40 CST 1976
+	Date :Tue Jul 11 17:53:40 CDT 1978
+	Date :Sat Apr 12 17:53:40 CST 1980
+	Date :Sat Nov 21 17:53:40 CST 1987
+	Date :Thu Aug 17 17:53:40 CDT 1989
+	Date :Sun Sep 16 17:53:40 CDT 1990
+	Date :Sun Oct 25 17:53:40 CST 1998
+	
 		        
  * @author janardhanbonu
  *
@@ -51,12 +67,19 @@ public class AgeCalculator {
 
 		AgeCalculator ac = new AgeCalculator();
 		ac.displayAge();
+		ac.sortDates();
+		ac.displayDates();
 	}
+	
 	
 	public void displayAge(){
 		Calendar bDate = new GregorianCalendar();
 		for (Date date : dates) {
 			bDate.setTime(date);
+			/* 
+			 * To keep it simple only the years difference is calculated
+			 * instead of calculating in time 
+			 */
 			int age = now.get(Calendar.YEAR) - bDate.get(Calendar.YEAR);
 			System.out.println("Date :"+date+"\t Age :"+age);
 		}
@@ -67,6 +90,17 @@ public class AgeCalculator {
 		GregorianCalendar cal = new GregorianCalendar();
 		cal.set(year, month, day);
 		return cal.getTime();
+	}
+	
+	public void sortDates() {
+		Arrays.sort(dates);
+		System.out.println("\n---- dates are sorted-----\n");
+	}
+	
+	private void displayDates() {
+		for (Date date : dates) {
+			System.out.println("Date :"+date.toString());
+		}
 	}
 	
 
