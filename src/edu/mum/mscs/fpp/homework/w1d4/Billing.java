b3d6637a01fa1cb93166d045563941ac255a0a09
janardhanbonu <janardhanbonu@gmail.com>
2015-03-22 23:32:05 -0500

    week1 day 4 uploaded

--- src/edu/mum/mscs/fpp/homework/w1d4/Billing.java
+++ src/edu/mum/mscs/fpp/homework/w1d4/Billing.java
@@ -17,24 +17,119 @@ package edu.mum.mscs.fpp.homework.w1d4;
 	
 	Write a main() method that tests all three overloaded methods. 
 
+	Output:
+	-------------billing 1-------------
+	Billing [photobookprice=250.0, photobooks=1, taxpercent=8, couponValuePercent=0.0, totalDue=270.0]
+	-------------billing 2-------------
+	Billing [photobookprice=250.0, photobooks=4, taxpercent=8, couponValuePercent=0.0, totalDue=1080.0]
+	-------------billing 3-------------
+	Billing [photobookprice=250.0, photobooks=12, taxpercent=8, couponValuePercent=2.5, totalDue=3237.5]
+
+	
+	
  * @author janardhanbonu
  *
  */
 public class Billing {
-
-	/**
-	 * 
-	 */
-	public Billing() {
-		// TODO Auto-generated constructor stub
+	
+	private float photobookprice=0;
+	private int photobooks = 1;
+	private final int taxpercent = 8;
+	private float couponValuePercent=0;
+	private float totalDue=0;
+	
+	public Billing(float photobookprice) {
+		this.photobookprice = photobookprice;
+		totalDue = calculateOneBookPrice();
 	}
 
+	public Billing(float photobookprice, int photobooks) {
+		this(photobookprice);
+		this.photobooks = photobooks;
+		totalDue = calculateBooksPrice();  
+	}
+
+	public Billing(float photobookprice, int photobooks, float couponValuePercent) {
+		this(photobookprice, photobooks);
+		this.couponValuePercent = couponValuePercent;
+		totalDue = calculateBooksPriceWithCoupon();
+	}
+
+	private float calculateOneBookPrice() {
+		return photobookprice + (0.08f * photobookprice);
+	}
+	
+	private float calculateBooksPrice() {
+		return calculateOneBookPrice() * photobooks;
+	}
+	
+	private float calculateBooksPriceWithCoupon() {
+		return  (calculateOneBookPrice() * photobooks) - couponValuePercent;
+	}
+
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Billing bill1 = new Billing(250);
+		Billing bill2 = new Billing(250,4);
+		Billing bill3 = new Billing(250,12,2.5f);
+		
+		
+		System.out.println("-------------billing 1-------------");
+		System.out.println(bill1.toString());
+		System.out.println("-------------billing 2-------------");
+		System.out.println(bill2.toString());
+		System.out.println("-------------billing 3-------------");
+		System.out.println(bill3.toString());
+		
 	}
+	
+
+
+	public float getTotalDue() {
+		return totalDue;
+	}
+	
+	public float getPhotobookprice() {
+		return photobookprice;
+	}
+
+	public void setPhotobookprice(float photobookprice) {
+		this.photobookprice = photobookprice;
+	}
+
+	public int getPhotobooks() {
+		return photobooks;
+	}
+
+	public void setPhotobooks(int photobooks) {
+		this.photobooks = photobooks;
+	}
+
+	public float getCouponValuePercent() {
+		return couponValuePercent;
+	}
+
+	public void setCouponValuePercent(float couponValuePercent) {
+		this.couponValuePercent = couponValuePercent;
+	}
+
+	public int getTaxpercent() {
+		return taxpercent;
+	}
+
+	@Override
+	public String toString() {
+		return "Billing [photobookprice=" + photobookprice + ", photobooks="
+				+ photobooks + ", taxpercent=" + taxpercent
+				+ ", couponValuePercent=" + couponValuePercent + ", totalDue="
+				+ totalDue + "]";
+	}
+
+	
+
 
 }
