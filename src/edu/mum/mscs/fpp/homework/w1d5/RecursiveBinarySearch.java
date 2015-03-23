57dcb4f9b6c0295c7c37b90847f7953fd47de382
janardhanbonu <janardhanbonu@gmail.com>
2015-03-23 01:27:19 -0500

    -u

--- src/edu/mum/mscs/fpp/homework/w1d5/RecursiveBinarySearch.java
+++ src/edu/mum/mscs/fpp/homework/w1d5/RecursiveBinarySearch.java
@@ -11,11 +11,32 @@ package edu.mum.mscs.fpp.homework.w1d5;
  */
 public class RecursiveBinarySearch {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-
-	}
+        
+        int[] ints = {3,5,14,15,35,80,91,99};
+        int index = binarySearch(ints,35,0,ints.length);
+        System.out.println("Found 35 at "+index+" index");
+        index = binarySearch(ints,80,0,ints.length);
+        System.out.println("Found 80 at "+index+" index");
+        index = binarySearch(ints,15,0,ints.length);
+        System.out.println("Found 15 at "+index+" index");
+    }
+	
+	public static int binarySearch(int[] ints, int searchNum, int iStart, int iEnd) {
+        
+        if (iStart < iEnd) {
+            int mid = iStart + (iEnd - iStart) / 2; 
+            if (searchNum < ints[mid]) {
+                return binarySearch(ints, searchNum, iStart, mid);
+                 
+            } else if (searchNum > ints[mid]) {
+                return binarySearch(ints, searchNum, mid+1, iEnd);
+                 
+            } else {
+                return mid;  
+            }
+        }
+        return iStart; 
+    }
 
 }
