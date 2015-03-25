47f6c35bb6af17cbd1407250eb3008eb9fb234c6
Janardhan Bonu <janardhanbonu@gmaill.com>
2015-03-25 10:24:57 -0500

    -u

---
+++ src/edu/mum/mscs/fpp/homework/practise/StringCompare.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package edu.mum.mscs.fpp.homework.practise;
+
+/**
+ * @author 984417
+ *
+ */
+public class StringCompare {
+
+	
+	
+	/**
+	 * 
+	 */
+	public StringCompare() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str1= null;
+		String str2 = null;
+		
+		str1.equals(str2);
+	}
+
+}
